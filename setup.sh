129655f61c27be7429d41a5c2f61c3760bd1887d
N1ck3nd <33940529+N1ck3nd@users.noreply.github.com>
2023-06-14 17:39:56 +0200

    Update setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+REPO_NAME="https://github.com/N1ck3nd/Accio-Workstation-Playbook.git"
+APT_PACKAGES="git curl python3 python3-pip"
+export ANSIBLE_FOLDER_PATH="$HOME/Accio-Workstation-Playbook"
+export PATH=$PATH:/$HOME/.local/bin
+
+echo ''
+echo 'PREPARE [Installing Ansible.] *********************************'
+echo ''
+sudo apt-get update -y >/dev/null 2>&1
+sudo apt-get install $APT_PACKAGES -y >/dev/null 2>&1
+python3 -m pip install $PIP_DEPENDENCIES > /dev/null 2>&1
+git clone $REPO_URL $ANSIBLE_FOLDER_PATH >/dev/null 2>&1
+
+## echo 'PREPARE [Installing requirements with Ansible-Galaxy.] ********'
+## echo ''
+## ansible-galaxy install -r $FOLDER_PATH/requirements.yml >/dev/null 2>&1
+
+echo 'RUN [Running playbook.] ***************************************'
+ansible-playbook $ANSIBLE_FOLDER_PATH/playbook.yml -i $ANSIBLE_FOLDER_PATH/inventory.yml
+
+echo -e '\e[35m[WARNING]: Please reboot the system to apply all changes.'
+echo ''
